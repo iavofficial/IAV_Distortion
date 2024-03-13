54d8140fe9fb465095874c598fa3cd9fafaa25b5
brotter <bjarne.rotter@iav.de>
2024-03-13 11:32:50 +0100

    Refactor DriverUI Klasse

--- UserInterface/driverUI.py
+++ UserInterface/driverUI.py
@@ -1,56 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask.views import View
-
-app = Flask(__name__)
-
-###--- Driver1 ---###
-#@app.route('/driver1')
-def home_driver1():
-    return render_template('driver_index.html')
-app.add_url_rule('/driver1', view_func=home_driver1)
-
-#@app.route('/slider', methods=['POST'])
-def slider_driver1():
-    value = request.form['value']
-    print(f"Driver1: Schieberegler-Wert: {value}")
-    return '', 204
-app.add_url_rule('/slider', view_func=slider_driver1, methods=['POST'])
-
-@app.route('/changeLane_left', methods=['POST'])
-def laneChange_left_driver1():
-    print("Driver1: Button << wurde gedrückt!")
-    # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 1 gedrückt wird.
-    return redirect(url_for('home_driver1'))
-
-@app.route('/changeLane_right', methods=['POST'])
-def changeLane_right_driver1():
-    print("Driver1: Button >> wurde gedrückt!")
-    # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 2 gedrückt wird.
-    return redirect(url_for('home_driver1'))
+from VehicleMovementManagement import DriverController
 
 
-# ###--- driver2 ---#
-# @app.route('/driver2')
-# def home_driver2():
-#     return render_template('driver_index.html')
-#
-# @app.route('/slider', methods=['POST'])
-# def slider_driver2():
-#     value = request.form['value']
-#     print(f"Driver2: Schieberegler-Wert: {value}")
-#     return '', 204
-#
-# @app.route('/changeLane_left', methods=['POST'])
-# def laneChange_left_driver2():
-#     print("Driver2: Button << wurde gedrückt!")
-#     # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 1 gedrückt wird.
-#     return redirect(url_for('home_driver2'))
-#
-# @app.route('/changeLane_right', methods=['POST'])
-# def changeLane_right_driver2():
-#     print("Driver2: Button >> wurde gedrückt!")
-#     # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 2 gedrückt wird.
-#     return redirect(url_for('home_driver2'))
+class DriverUI:
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    def __init__(self, mapOfUuids, name=__name__):
+        self.app = Flask(name)
+        self.uuids = mapOfUuids
+        return
+
+    def run(self):
+        def home_driver(player):
+            return render_template('driver_index.html', my_var=player)
+        self.app.add_url_rule('/driver/<player>', 'home_driver', view_func=home_driver)
+
+        def slider_driver(player):
+            value = request.form['value']
+            #print(f"Driver{player} : Slider value: {value}")
+            request_speed_change()
+            return '', 204
+        self.app.add_url_rule('/slider/<player>', 'slider_driver', methods=['POST'], view_func=slider_driver)
+
+        # @self.app.route('/changeLane_left', methods=['POST'])
+        def change_lane_left(player):
+            #uuid = self.uuids{player}
+            print(f"Driver{player}: Button << pressed!")
+            #request_speed_change(uuid, 'left')
+            # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 1 gedrückt wird.
+            return redirect(url_for('home_driver', player=player))
+        self.app.add_url_rule('/change_lane_left/<player>', 'change_lane_left', methods=['POST'], view_func=change_lane_left)
+
+        # @self.app.route('/changeLane_right', methods=['POST'])
+        def change_lane_right(player):
+            print(f"Driver{player}: Button >> pressed!")
+            # Hier können Sie Ihren Python-Code hinzufügen, der ausgeführt werden soll, wenn der Button 2 gedrückt wird.
+            return redirect(url_for('home_driver', player=player))
+        self.app.add_url_rule('/changeLane_right/<player>', 'change_lane_right', methods=['POST'], view_func=change_lane_right)
+
+        self.app.run(debug=True, host='0.0.0.0')
