064e432785e700fd3e09fbced0435ddb27d3764d
brotter <bjarne.rotter@iav.de>
2024-03-26 15:49:46 +0100

    added function to remove vehicles from vehicle list and map_of_uuids

--- EnvironmentManagement/EnvironmentManager.py
+++ EnvironmentManagement/EnvironmentManager.py
@@ -1,5 +1,6 @@
 from DataModel.Vehicle import Vehicle
 
+
 class EnvironmentManager:
 
     def __init__(self):
@@ -16,6 +17,11 @@ class EnvironmentManager:
         return
 
     def _find_active_anki_cars(self):
+        # Funktion zum Fahrzeuge Suchen und Verbinden
+        # BLE device suchen
+        # mit Device verbinden
+        # vehicle initialisieren
+
         vehicle1 = Vehicle("12:34:56", "dummyCar1")
         vehicle2 = Vehicle("78:90:01", "dummyCar2")
         self._active_anki_cars = [vehicle1, vehicle2]
@@ -24,7 +30,15 @@ class EnvironmentManager:
     def get_vehicle_list(self):
         return self._active_anki_cars
 
+    def remove_vehicle(self, uuid_to_remove: str):
+        player_to_remove = ''
+        for player, uuid in self._player_uuid_map.items():
+            if uuid == uuid_to_remove:
+                player_to_remove = player
 
+        if player_to_remove is not '':
+            del self._player_uuid_map[player_to_remove]
 
-
+        self._active_anki_cars = [vehicle for vehicle in self._active_anki_cars if vehicle.uuid != uuid_to_remove]
+        return
 
