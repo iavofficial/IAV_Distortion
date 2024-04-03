4c876dde349413cf3b8ea081f04c6f87655d1450
DanielBierkarre <daniel.bierkarre@iav.de>
2024-04-03 09:33:13 +0200

    Erzeuge Befehlsfunktionen und Aufrufe von async tasks

--- VehicleManagement/VehicleController.py
+++ VehicleManagement/VehicleController.py
@@ -1,29 +1,64 @@
 import asyncio
+import struct
 from bleak import BleakClient, BleakScanner
-
-
-async def scan_for_cars():
-    ble_devices = await BleakScanner.discover(return_adv=True)
-
-    for d, a in ble_devices.values():
-        if d.name is not None and "Drive" in d.name:
-            print()
-            print(d)
-            print("-" * len(str(d)))
-            print(a)
-    filtered_anki_cars = [d for d in ble_devices.values() if d[0].name is not None and "Drive" in d[0].name]
-
-    async with BleakClient(filtered_anki_cars[0][0].address) as anki_car:
-        result = anki_car.is_connected
-        await anki_car.disconnect()
-        result = anki_car.is_connected
-    return filtered_anki_cars
 
 
 class VehicleController:
     def __init__(self):
-        self.anki_vehicles = []
+        self._active_devices = []
+        self._connected_cars = {}
         return
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(scan_for_cars())
+    def __del__(self):
+        del self._active_devices
+        del self._connected_cars
+
+    def __str__(self):
+        return "Connected Cars" + str(self._connected_cars)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __len__(self):
+        return len(self._connected_cars)
+
+    async def scan_for_anki_cars(self):
+        ble_devices = await BleakScanner.discover(return_adv=True)
+        self._active_devices = [d for d in ble_devices.values() if d[0].name is not None and "Drive" in d[0].name]
+        if self._active_devices:
+            await self._connect_to_anki_cars()
+
+    async def _connect_to_anki_cars(self):
+        for anki_car in self._active_devices:
+            connected_car = BleakClient(anki_car[0].address)
+            await connected_car.connect()
+            if connected_car.is_connected():
+                self._connected_cars[connected_car.address] = connected_car
+
+    def change_speed(self, uuid: str):
+        command = struct.pack("<BHHB", 0x24, 800, 1000, 0x01)
+        asyncio.create_task(self.__send_command(command, uuid))
+
+    def change_lane(self, uuid: str):
+        command = struct.pack("<BHHf", 0x25, 800, 1000, -44.5)
+        asyncio.create_task(self.__send_command(command, uuid))
+
+    def do_u_turn(self, uuid: str):
+        command = struct.pack("<BHH", 0x32, 0x03, 0x00)
+        asyncio.create_task(self.__send_command(command, uuid))
+
+    def set_sdk_mode_to(self, uuid: str, value: bool):
+        command = struct.pack("<BBB", 0x90, 0x01, 0x01)
+        asyncio.create_task(self.__send_command(command, uuid))
+
+    def __disconnect_from(self, uuid: str):
+        command = struct.pack("<B", 0xd)
+        asyncio.create_task(self.__send_command(command, uuid))
+        del self._connected_cars[uuid]
+
+    async def __send_command(self, command: bytes, uuid: str):
+        final_command = struct.pack("B", len(command)) + command
+        await self._connected_cars[uuid].write_gatt_char("BE15BEE1-6186-407E-8381-0BD89C4D8DF4", final_command, None)
+
+    async def __receive_data(self, uuid: str):
+        await self._connected_cars[uuid].read_gatt_char()
