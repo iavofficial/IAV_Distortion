fbd97c8ddad993a8b9a987cb70430d93174cba87
iav-DaBi <daniel.bierkarre@iav.de>
2024-04-30 12:49:44 +0200

    Add testcases

--- test/IntegrationTest/Vehicle_Test.py
+++ test/IntegrationTest/Vehicle_Test.py
@@ -1,31 +1,28 @@
 from time import sleep
 from unittest.mock import Mock
 
-import sys
-import os
-sys.path.append(os.path.abspath('../../src'))
-
 from DataModel.Vehicle import Vehicle
 from VehicleManagement.VehicleController import VehicleController
 
 
-dummy_uuid = "FA:14:67:0F:39:FE"
+dummy_uuid = "F1:76:08:08:C1:00" #rot
 
 
 def test_on_driving_data_change() -> None:
-    print('1')
     # Arrange
     vehicle_controller = VehicleController()
     mut = Vehicle(dummy_uuid, vehicle_controller)
-    mut.player = "1"
+    if mut.initiate_connection(dummy_uuid) is False:
+        assert False
 
+    mut.player = "1"
     mut.set_driving_data_callback(return_driving_data)
 
     # Act
     mut.hacking_scenario = "test_scenario"
-    mut.speed_request = 200
-    sleep(5)
-    mut.speed_request = 250
+    mut.speed_request = 100
+    sleep(4)
+    mut.speed_request = 75
     sleep(2)
     mut.speed_request = 0
     del mut
@@ -34,6 +31,3 @@ def test_on_driving_data_change() -> None:
 def return_driving_data(data: dict) -> None:
     print(f"return_driving_data: {data}")
     return
-
-
-test_on_driving_data_change()
