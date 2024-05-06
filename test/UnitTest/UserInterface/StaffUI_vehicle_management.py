473229e38f06b13fe81a0fe3dafc29a7e8eb283f
brotter <bjarne.rotter@iav.de>
2024-05-06 10:39:38 +0200

    adapt StaffUi Mock to password usage

--- test/UnitTest/UserInterface/StaffUI_vehicle_management.py
+++ test/UnitTest/UserInterface/StaffUI_vehicle_management.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
 from flask import Flask
+import os
 from flask_socketio import SocketIO
 from UserInterface.StaffUI import StaffUI
 
@@ -29,17 +30,22 @@ environment_mng_mock.find_unpaired_anki_cars.return_value = ['11:22:33:44', '55:
 environment_mng_mock.add_vehicle.return_value = None
 
 
-def build_ui():
+def build_ui(admin_password: str):
     app = Flask('IAV_Distortion', template_folder='../../../src/UserInterface/templates',
                 static_folder='../../../src/UserInterface/static')
     socketio = SocketIO(app, cors_allowed_origins="*", async_mode=None)
 
     staff_ui = StaffUI(map_of_uuids=player_uuid_map, cybersecurity_mng=cybersecurity_mng_mock, socketio=socketio,
-                       environment_mng=environment_mng_mock)
+                       environment_mng=environment_mng_mock, password=admin_password)
     staff_ui_blueprint = staff_ui.get_blueprint()
 
     app.register_blueprint(staff_ui_blueprint, url_prefix='/staff')
     socketio.run(app, debug=True, host='0.0.0.0', allow_unsafe_werkzeug=True)
 
 
-build_ui()
+admin_pwd = os.environ.get('ADMIN_PASSWORD')
+if admin_pwd is None:
+    print("WARNING!!! No admin password supplied via Environement variable. Using '123' as default password!")
+    admin_pwd = '123'
+
+build_ui(admin_pwd)
