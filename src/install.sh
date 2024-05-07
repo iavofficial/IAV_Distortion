59d66a77aba2b57b3c224d2b3fd03fddce1be84b
brotter <bjarne.rotter@iav.de>
2024-05-07 15:19:47 +0200

    add set password in pipenv .env file during installation process

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,61 @@
 
 working_directory=$(pwd)
 
+# set password, this has to be adapted in the future, so that the password won't be stored in clear text anywhere in the system
+env_file="$working_directory/.env"
+
+function configure_password() {
+    while true; do
+        while true; do
+            read -s -p "Enter password (at least 6 charakters): " password
+            echo
+            if [[ ${#password} -ge 6 ]]; then
+                # accept input, since it has at least 6 charakters
+                break
+            else
+                echo "Password must be at least 6 characters long. Please try again."
+            fi
+        done
+            read -s -p "Confirm password: " password2
+            echo
+            if [ "$password" = "$password2" ]; then
+                echo "Passwords match. Password safed!"
+                if grep -q "ADMIN_PASSWORD=" "$env_file"; then
+                    # If exists, use sed to replace its value
+                    sed -i "s/^ADMIN_PASSWORD=.*/ADMIN_PASSWORD=$password/" $env_file
+                else
+                    # If doesn't exist, append it to the env_file
+                    echo "ADMIN_PASSWORD=$password" >> $env_file
+                fi
+                break
+            else
+                echo "Passwords do not match. Please try again."
+            fi
+    done
+}
+
+if [ -f "$env_file" ]; then
+    echo "creating .env file"
+    if grep -q "ADMIN_PASSWORD=" "$env_file"; then
+        read -p "Password already set. Do you want to set a new one? (y/n) " -n 1 -r
+        echo    # move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            configure_password
+        else
+            echo "Using existing password."
+        fi
+    else
+        echo "No password configured yet, please set one..."
+        configure_password
+    fi
+else 
+    echo "Die Datei $env_file existiert nicht. Sie wird erzeugt..."
+    touch "$env_file"
+    echo "Die Datei $env_file wurde erfolgreich erzeugt."
+    configure_password
+fi
+
 # install pipenv
 pipenv install --skip-lock
 
