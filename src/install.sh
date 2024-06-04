c07c0e45b9f34a175a483144917cdccabc8b60fc
brotter <bjarne.rotter@iav.de>
2024-06-04 14:28:45 +0200

    split install script. add script to get_dependencies (install pipenv, download external resources)

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,9 @@
 
 working_directory=$(pwd)
 
+# make all utility scripts in current folder executable
+find .. -type f -iname "*.sh" -exec chmod +x {} \;
+
 # set password, this has to be adapted in the future, so that the password won't be stored in clear text anywhere in the system
 env_file="$working_directory/.env"
 
@@ -57,48 +60,8 @@ else
     configure_password
 fi
 
-# move pipfile to current working directory
-mv ../Pipfile .
-
-# install pipenv
-pipenv install --skip-lock
-
-# get jquery dependencies
-ui_resources_directory="UserInterface/static/external_resources"
-
-# list of required resources:
-jquery_version="3.7.1"
-jquery_url="https://code.jquery.com/jquery-$jquery_version.min.js"
-jquery_directory=$ui_resources_directory
-
-socketio_version="4.7.5"
-socketio_url="https://cdnjs.cloudflare.com/ajax/libs/socket.io/$socketio_version/socket.io.js"
-socketio_directory="$ui_resources_directory/socketio/$socketio_version"
-
-# remove old files if exist
-if [ -d "$ui_resources_directory" ]; then
-    rm -rf $ui_resources_directory
-fi
-
-# recreate directories
-if [ ! -d "$jquery_directory" ]; then
-    mkdir -p $jquery_directory
-fi
-if [ ! -d "$socketio_directory" ]; then
-    mkdir -p $socketio_directory
-fi
-
-# download resources - resources must be added here!
-if [ $(command -v wget) ]; then
-    wget -P "$jquery_directory" "$jquery_url"
-    wget -P "$socketio_directory" "$socketio_url"
-elif [ $(command -v curl) ]; then
-    cd "$working_directory/$jquery_directory" && { curl -O "$jquery_url" ; cd -; }
-    cd "$working_directory/$socketio_directory" && { curl -O "$socketio_url" ; cd -; }
-else
-    echo -e "\033[0;31mError: wget or curl requiered! \033[0m"
-    read
-fi
+# get dependencies
+$working_directory/get_dependencies.sh
 
 # create desktop item
 current_user=$(whoami)
@@ -116,9 +79,6 @@ echo "Terminal=true" >> "$desktop_item"
 echo "Type=Application" >> "$desktop_item"
 echo "Name[de_DE]=IAV Distortion" >> "$desktop_item"
 echo "Path=$working_directory" >> "$desktop_item"
-
-# make all utility scripts in current folder executable
-find . -type f -iname "*.sh" -exec chmod +x {} \;
 
 # add add run_IAV-Distortion.sh to autostart
 check_cronjobs=$(crontab -l | grep "run_IAV-Distortion.sh")
