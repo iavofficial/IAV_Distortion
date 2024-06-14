328fcf01d6ec3f3f642aa1e43885a936552cb7f9
brotter <bjarne.rotter@iav.de>
2024-06-14 16:35:59 +0200

    add getting configured pictures for virtual vehicles from configuration file

--- src/UserInterface/CarMap.py
+++ src/UserInterface/CarMap.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from EnvironmentManagement.EnvironmentManager import EnvironmentManager
+from EnvironmentManagement.ConfigurationHandler import ConfigurationHandler
+
 
 class CarMap:
     """
@@ -12,7 +14,8 @@ class CarMap:
     """
     def __init__(self, environment_manager: EnvironmentManager):
         self.carMap_blueprint: Blueprint = Blueprint(name='carMap_bp', import_name='carMap_bp')
-        self._environment_manager = environment_manager
+        self._environment_manager: EnvironmentManager = environment_manager
+        self.config_handler = ConfigurationHandler()
 
         def home_car_map():
             """
@@ -27,7 +30,10 @@ class CarMap:
                 Returns a Response object representing the car map page.
             """
             track = environment_manager.get_track().get_as_list()
-            return render_template("car_map.html", track=track, color_map=environment_manager.get_car_color_map())
+            car_pictures = self.config_handler.get_configuration()["virtual_cars_pics"]
+            return render_template("car_map.html", track=track, car_pictures=car_pictures,
+                                   color_map=environment_manager.get_car_color_map())
+
         self.carMap_blueprint.add_url_rule("", "home_car_map", view_func=home_car_map)
 
     def get_blueprint(self) -> Blueprint:
