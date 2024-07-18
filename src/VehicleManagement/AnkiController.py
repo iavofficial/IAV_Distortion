40513cc8511c4f3a415c0bbde32b7cd9f89b023f
brotter <bjarne.rotter@iav.de>
2024-07-18 15:06:11 +0200

    add AnkiController unit test 'test_change_speed_to_multiple_inputs'

--- src/VehicleManagement/AnkiController.py
+++ src/VehicleManagement/AnkiController.py
@@ -1,3 +1,11 @@
+# Copyright 2024 IAV GmbH
+#
+# This file is part of the IAV-Distortion project an interactive
+# and educational showcase designed to demonstrate the need
+# of automotive cybersecurity in a playful, engaging manner.
+# and is released under the "Apache 2.0". Please see the LICENSE
+# file that should have been included as part of this package.
+#
 import asyncio
 from asyncio import Task
 import struct
