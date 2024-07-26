e5366f36d680a99c8c0662327e8081d7bb66189e
brotter <bjarne.rotter@iav.de>
2024-07-26 15:29:11 +0200

    add test_start_stop_auto_discover_anki_cars()

--- test/UnitTest/VehicleManagement/FleetController_Test.py
+++ test/UnitTest/VehicleManagement/FleetController_Test.py
@@ -130,11 +130,56 @@ async def test_auto_discover_anki_vehicles_invalid_callback() -> None:
     with patch('VehicleManagement.FleetController.FleetController.stop_auto_discover_anki_cars') as mock_stop_auto_discover:
         fleet_controller = FleetController(test_config_handler)
         fleet_controller.set_add_anki_car_callback(None)
-        fleet_controller.scan_for_anki_cars = AsyncMock(return_value=["uuid1"])
+        test_uuid = "uuid1"
+        fleet_controller.scan_for_anki_cars = AsyncMock(return_value=[test_uuid])
 
         # Act
         await fleet_controller.start_auto_discover_anki_cars()
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.1)
 
         # Assert
         mock_stop_auto_discover.assert_called_once()
+
+
+@pytest.mark.slow
+@pytest.mark.asyncio
+async def test_start_stop_auto_discover_anki_cars() -> None:
+    """
+    Test if FleetController.start_auto_discover_anki_cars() starts the auto discovery service AND if
+    FleetController.stop_auto_discover_anki_cars() stops the auto discovery service.
+
+    Precondition:
+    - no special preconditions required
+
+    Expectation:
+    - after starting the auto_discover service the attribute __auto_connect_anki_cars_task is an asyncio.Task
+    - after starting the auto_discover service the add_anki_car_callback is called at least once
+
+    - after stopping the auto_discover service the add_anki_car_callback is not called anymore
+    - after stopping the auto_discover service the attribute __auto_connect_anki_cars_task is reseted to None
+    """
+    # Arrange
+    config_handler = ConfigurationHandler('../src/config_file.json')  # use default config_file
+
+    fleet_ctrl = FleetController(config_handler=config_handler)
+    mock_add_car_callback = AsyncMock()
+    fleet_ctrl.set_add_anki_car_callback(mock_add_car_callback)
+    fleet_ctrl.scan_for_anki_cars = AsyncMock(return_value=["uuid1"])
+
+    # Act 1
+    await fleet_ctrl.start_auto_discover_anki_cars()
+    await asyncio.sleep(2)  # wait a short period to allow the auto discovery service run at least once
+
+    # Assert 1
+    assert isinstance(fleet_ctrl._FleetController__auto_connect_anki_cars_task, asyncio.Task)
+    callback_count_before_stop = mock_add_car_callback.call_count
+    mock_add_car_callback.assert_called()
+
+    # Act 2
+    fleet_ctrl.stop_auto_discover_anki_cars()
+    await asyncio.sleep(7)  # wait for longer as the while loop will be performed to ensure it was not executed again
+    callback_count_after_stop = mock_add_car_callback.call_count
+
+    # Assert 2
+    assert callback_count_after_stop == callback_count_before_stop
+    assert fleet_ctrl._FleetController__auto_connect_anki_cars_task is None
