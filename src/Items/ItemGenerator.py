f154cc818b18c7d2bb27cd27fdac2b25faceccfa
Mika Braunschweig <mika.braunschweig@iav.de>
2024-08-30 10:56:41 +0200

    Periodically generate items
    This is done so items generate endlessy

--- src/Items/ItemGenerator.py
+++ src/Items/ItemGenerator.py
@@ -1,3 +1,6 @@
+import asyncio
+from asyncio import Task
+
 from CyberSecurityManager.CyberSecurityManager import CyberSecurityManager
 from DataModel.Effects.HackingProtection import HackingProtection
 from Items.Item import Item
@@ -11,6 +14,7 @@ class ItemGenerator:
         self._item_collision_detection: ItemCollisionDetector = item_collision_detection
         self._cyber_security_manager: CyberSecurityManager = cyber_security_manager
         self._track: FullTrack | None = track
+        self._item_generation_task: Task | None = None
         for _ in range(0, 3):
             self._item_collision_detection.add_item(self.generate_item())
 
@@ -23,3 +27,11 @@ class ItemGenerator:
 
     def generate_item(self) -> Item:
         return Item(self._track, HackingProtection(self._cyber_security_manager))
+
+    async def start_item_generation(self) -> None:
+        self._item_generation_task = asyncio.create_task(self._generate_item_task_function())
+
+    async def _generate_item_task_function(self) -> None:
+        while True:
+            await asyncio.sleep(30)
+            self._item_collision_detection.add_item(self.generate_item())
