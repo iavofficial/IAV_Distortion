16f6f11df73e5af6e25cd65cdce3160fe69f3121
jan <j.meitrodt@ostfalia.de>
2024-11-01 14:43:44 +0100

    Backend Implementation 1.0

--- src/UserInterface/DriverUI.py
+++ src/UserInterface/DriverUI.py
@@ -18,6 +18,7 @@ from socketio import AsyncServer
 
 from DataModel.Vehicle import Vehicle
 from DataModel.Effects.SwitchCars import SwitchCars
+from DataModel.Effects.SwitchCars import SwitchCars
 from EnvironmentManagement.EnvironmentManager import EnvironmentManager
 from EnvironmentManagement.ConfigurationHandler import ConfigurationHandler
 from VehicleMovementManagement.BehaviourController import BehaviourController
