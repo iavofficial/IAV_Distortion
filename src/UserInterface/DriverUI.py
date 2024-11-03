8098000c464721ef4fef40458e5d6f4d137c50d2
jan <j.meitrodt@ostfalia.de>
2024-11-03 15:19:58 +0100

    still throws errors when activated

--- src/UserInterface/DriverUI.py
+++ src/UserInterface/DriverUI.py
@@ -206,10 +206,11 @@ class DriverUI:
             return
         
         @self._sio.on('switch_cars')
-        def switch_cars(data: dict) -> None:
+        def switch_cars(sid, data: dict) -> None:
             player = data["player"]
             car = self.environment_mng.get_vehicle_by_player_id(player)
             SwitchCars.switch(environment_mng, car)
+            return
 
     def update_driving_data(self, driving_data: dict) -> None:
         self.__run_async_task(self.__emit_driving_data(driving_data))
