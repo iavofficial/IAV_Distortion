31db309d9aa4bdf862a550eb64cbd905b2ab5dc0
jan <j.meitrodt@ostfalia.de>
2024-11-13 16:54:11 +0100

    Private Attributes in Driver.py

--- src/DataModel/Driver.py
+++ src/DataModel/Driver.py
@@ -3,22 +3,22 @@ from DataModel.Vehicle import Vehicle
 
 class Driver:
     def __init__(self, player_id: str) -> None:
-        self.player: str = player_id
-        self.score: float = 0
-        self.in_physical_vehicle: bool = False
+        self.__player: str = player_id
+        self.__score: float = 0
+        self.__in_physical_vehicle: bool = False
         return
     
     def get_score(self) -> float:
-        return self.score
+        return self.__score
     
     def increase_score(self, amount: float) -> None:
-        self.score += amount
+        self.__score += amount
 
     def set_is_in_physical_vehicle(self, b: bool) -> None:
-        self.in_physicalvehicle = b
+        self.__in_physical_vehicle = b
 
     def get_is_in_physical_vehicle(self) -> bool:
-        return self.in_physical_vehicle
+        return self.__in_physical_vehicle
     
     def get_player_id(self) -> str:
-        return self.player
+        return self.__player
