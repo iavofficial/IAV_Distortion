60e5ad01ee6e8975ceeeccbf0f7b71f2ef63babd
LeonUwe <l.rueckert@ostfalia.de>
2024-11-15 09:41:35 +0100

    Added Tapping Contest backend and frontend via ChatGPT prompting. -LeonR

---
+++ src/Minigames/Tapping_Contest.py
@@ -0,0 +1,79 @@
+from quart import Blueprint
+import time
+import asyncio
+from socketio import AsyncServer
+from Minigames.Minigame import Minigame
+
+class Tapping_Contest(Minigame):
+    def __init__(self, sio: AsyncServer, blueprint : Blueprint, name=__name__):
+        super().__init__(sio, blueprint, name)
+        self._players : dict = {}
+        self._clicks : dict = {}
+        self._start_time : time = None
+
+        @self._sio.on('join_game')
+        async def on_join_game(sid: str, data):
+            player_id = data['player_id']
+            await self._sio.enter_room(sid, "Tapping_Contest")
+            await self._sio.emit('joined', {'player_id': player_id}, room=player_id)
+
+        @self._sio.on('start_game')
+        async def start_game(sid: str, data):
+            player1 = data['player1']
+            player2 = data['player2']
+            winner = await self._play(player1, player2)
+            print(f'Winner: {winner}')
+
+        @self._sio.on('click')
+        async def handle_click(sid: str, data):
+            player_id = data['player_id']
+            await self._record_click(player_id)
+
+    async def _play(self, player1: str, player2: str) -> str:
+        self._players = {player1: 0, player2: 0}
+        self._clicks = {player1: 0, player2: 0}
+        self._start_time = time.time() + 3  # 3-second countdown
+
+        await asyncio.sleep(2)
+        # Notify players of the countdown
+        await self._broadcast_countdown()
+
+        # Start the 10-second game timer after countdown
+        await self._start_game()
+
+        await asyncio.sleep(10)  # Game duration
+
+        player1, player2 = self._players.keys()
+        clicks1, clicks2 = self._clicks[player1], self._clicks[player2]
+
+        winner = None
+        if clicks1 > clicks2:
+            winner = player1
+        elif clicks2 > clicks1:
+            winner = player2
+
+        self._players.clear()
+        self._clicks.clear()
+        return winner
+
+    async def _broadcast_countdown(self):
+        for sec in range(3, -1, -1):
+            await self._sio.emit('countdown', {'count': sec}, room = "Tapping_Contest")
+            await asyncio.sleep(1)
+
+    async def _start_game(self):
+        await self._sio.emit('start_game', {'message': 'Game started!'}, room = "Tapping_Contest")
+
+    async def _record_click(self, player_id: str):
+        if player_id in self._clicks:
+            self._clicks[player_id] += 1
+            clicks = self._clicks[player_id]
+            elapsed = max(time.time() - self._start_time, 0)
+            cps = clicks / elapsed if elapsed > 0 else 0
+            await self._sio.emit(
+                'update_clicks',
+                {'player_id': player_id, 'clicks': clicks, 'cps': cps},
+                room="Tapping_Contest"
+            )
+
+
