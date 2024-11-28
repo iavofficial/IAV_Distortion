224979e473d7ab92b6390197f518e11f164b058e
LeonUwe <l.rueckert@ostfalia.de>
2024-11-28 15:13:54 +0100

    Removed duplicate socketIO emit 'minigame_winner' in MinigameUI. -LeonR

--- src/UserInterface/MinigameUI.py
+++ src/UserInterface/MinigameUI.py
@@ -173,11 +173,6 @@ class Minigame_UI:
             else:
                 winner = await minigame_task
 
-            await self._sio.emit('minigame_winner',
-                                 {'minigame': minigame_object.get_name(),
-                                  'winner': winner,
-                                  'owner_of_physical_vehicle_at_start': owner_of_physical_vehicle_at_start})
-            print(f"Winner of minigame {minigame_object.get_name()} is player {winner}.")
             winner = await minigame_task
 
             await self._sio.emit('minigame_winner',
