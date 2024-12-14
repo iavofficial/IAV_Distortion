3148fac6562ee27b24ca4ca86a75f31962250c70
LeonUwe <l.rueckert@ostfalia.de>
2024-12-14 15:41:23 +0100

    More flake8 fixes. -LeonR

--- src/UserInterface/DriverUI.py
+++ src/UserInterface/DriverUI.py
@@ -21,9 +21,7 @@ from EnvironmentManagement.EnvironmentManager import EnvironmentManager
 from EnvironmentManagement.ConfigurationHandler import ConfigurationHandler
 from VehicleMovementManagement.BehaviourController import BehaviourController
 
-from UserInterface.MinigameUI import Minigame_UI
 from Minigames.Minigame_Controller import Minigame_Controller
-from Minigames.Minigame import Minigame
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +39,7 @@ class DriverUI:
         self.environment_mng: EnvironmentManager = environment_mng
         self.config_handler: ConfigurationHandler = ConfigurationHandler()
 
-        self.minigame_players : set = set()
+        self.minigame_players: set = set()
 
         self.__latest_driver_heartbeats: dict[str, float] = {}
         self.__checking_heartbeats_flag: bool = False
@@ -211,11 +209,13 @@ class DriverUI:
                 return
             target_vehicle_id = vehicle.vehicle_in_proximity
             if target_vehicle_id is None:
-                logger.warn(f"Driver UI: No target vehicle id for player {player} driving {vehicle.get_vehicle_id()} could be found. Ignoring the switch request.")
+                logger.warn(f"Driver UI: No target vehicle id for player {player} driving {vehicle.get_vehicle_id()} \
+                    could be found. Ignoring the switch request.")
                 return
             target_vehicle = self.environment_mng.get_vehicle_by_vehicle_id(target_vehicle_id)
             if target_vehicle is None:
-                logger.warn(f"Driver UI: No target vehicle for player {player} driving {vehicle.get_vehicle_id()} with target_vehicle_id {target_vehicle_id} could be found. Ignoring the switch request.")
+                logger.warn(f"Driver UI: No target vehicle for player {player} driving {vehicle.get_vehicle_id()} with \
+                    target_vehicle_id {target_vehicle_id} could be found. Ignoring the switch request.")
                 return
             target_player = target_vehicle.get_player_id()
             minigame_players = []
@@ -228,10 +228,12 @@ class DriverUI:
                 play_random_available_minigame(*minigame_players)
 
             if minigame_task is None:
-                logger.warning(f"DriverUI: The minigame for player {player} and player {target_player} could not be started for some reason. Ignoring the request.")
+                logger.warning(f"DriverUI: The minigame for player {player} and player {target_player} could not be \
+                    started for some reason. Ignoring the request.")
                 return
             winner = await minigame_task
-            logger.debug(f"DriverUI: The player {player} has won a minigame that was initiated by a hack of vehicle {target_vehicle_id}.")
+            logger.debug(f"DriverUI: The player {player} has won a minigame that was initiated by a \
+                hack of vehicle {target_vehicle_id}.")
             if winner is None or winner == target_player or winner == "":
                 return
 
