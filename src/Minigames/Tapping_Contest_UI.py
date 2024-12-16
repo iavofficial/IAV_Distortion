01984c7073ecf048d686e573c41703a5aeafb996
LeonUwe <l.rueckert@ostfalia.de>
2024-12-16 17:21:48 +0100

    #73 merge 2. -LeonR

--- src/Minigames/Tapping_Contest_UI.py
+++ src/Minigames/Tapping_Contest_UI.py
@@ -24,10 +24,11 @@ class Tapping_Contest_UI(Minigame):
         async def on_join_game(sid: str, data):
             player_id = data['player_id']
             if player_id in self._players:
-                await self._sio.enter_room(sid, "Tapping_Contest")
-                await self._sio.emit('joined', {'player_id': player_id}, room=player_id)
+                await self._sio.enter_room(sid, self._room)
+                await self._sio.emit('Tapping_Contest_joined', {'player_id': player_id,
+                                                                'namespace': namespace}, room=self._room)
 
-        @self._sio.on('Tapping_Contest_click')
+        @self._sio.on('Tapping_Contest_click', namespace='/' + namespace)
         async def handle_click(sid: str, data):
             player_id = data['player_id']
             await self._record_click(player_id)
@@ -50,8 +51,10 @@ class Tapping_Contest_UI(Minigame):
 
         self._players.clear()
         self._players.append(players[0])
+        self._room = players[0]
         if len(players) > 1:
             self._players.append(players[1])
+            self._room += players[1]
         return self.get_players()
 
     async def _play(self) -> str:
@@ -82,7 +85,7 @@ class Tapping_Contest_UI(Minigame):
 
     async def _start_game(self):
         self._game.start()
-        await self._sio.emit('start_game', {'game-length': self._game_length}, room="Tapping_Contest")
+        await self._sio.emit('start_game', {'game-length': self._game_length}, room=self._room)
 
     async def _record_click(self, player_id: str):
         if player_id not in self._players:
@@ -95,11 +98,11 @@ class Tapping_Contest_UI(Minigame):
         await self._sio.emit(
             'update_clicks',
             {'player_id': player_id, 'clicks': self._game.get_clicks()[player_index], 'cps': cps},
-            room="Tapping_Contest"
-         )
+            room=self._room
+        )
 
     async def _send_tie(self):
-        await self._sio.emit('tie', to="Tapping_Contest")
+        await self._sio.emit('tie', to=self._room)
 
     def description(self) -> str:
         return f"Whoever clicks more times in {self._game_length} seconds, wins."
