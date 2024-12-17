8168b1254e3392cc7e0c756d4397a2b6083204f0
id975298 <m.poelig@ostfalia.de>
2024-12-17 12:52:13 +0100

    used assertIsNone for assertions

--- test/UnitTest/UserInterface/CarMap_Test.py
+++ test/UnitTest/UserInterface/CarMap_Test.py
@@ -1,4 +1,5 @@
 from unittest.mock import MagicMock, patch
+import unittest
 
 import pytest
 
@@ -8,7 +9,7 @@ from LocationService.Trigo import Position
 from UserInterface.CarMap import CarMap
 
 
-class TestVehicleProximity:
+class TestVehicleProximity(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def setup_mocks(self):
@@ -58,5 +59,5 @@ class TestVehicleProximity:
         self.car_map.check_virtual_vehicle_proximity("car1", {"x": 100, "y": 100})
 
         # Assert
-        assert self.vehicle1.vehicle_in_proximity is None
-        assert self.vehicle2.vehicle_in_proximity is None
+        self.assertIsNone(self.vehicle1.vehicle_in_proximity)
+        self.assertIsNone(self.vehicle2.vehicle_in_proximity)
